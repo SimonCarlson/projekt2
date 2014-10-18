b0bf6707f4e83ccadd2a9807b7db31cf4a001b59
Simon Carlson <cimon.carlson@gmail.com>
2014-10-18 13:12:33 +0200

    Inception

---
+++ src/main/java/FileSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ */
+public interface FileSystem {
+
+  public void mkdir(String path);
+  
+  public void touch(String path);
+  
+  public void append(String path, String data);
+  
+  public String lsByTime(String path);
+  
+  public String lsByName(String path);
+  
+  public String du(String path);
+  
+  public String cat(String path);
+}
---
+++ src/main/java/MiniFs.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ */
+public class MiniFs implements FileSystem {
+
+  private final INodeDirectory root;
+
+  public MiniFs() {
+    root = new INodeDirectory("/");
+  }
+  
+  @Override
+  public void mkdir(String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void touch(String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void append(String path, String data) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public String lsByTime(String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public String lsByName(String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public String du(String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public String cat(String path) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+  
+}
